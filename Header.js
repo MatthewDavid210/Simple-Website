2a8a351de8a41e0243d50e6927150d87ea1afc07
MatthewDavid210 <121847464+MatthewDavid210@users.noreply.github.com>
2023-10-30 18:59:58 -0500

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+let font;
+
+function preload() {
+  font = loadFont('fonts/Edu_QLD.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textFont(font);
+  textSize(60);
+  fill(0);
+  text("Matthew David", 50, 120); //
+}
