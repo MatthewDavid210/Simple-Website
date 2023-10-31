cc60f45c252ae18ca3d66ffdaa540450f42d4970
MatthewDavid210 <121847464+MatthewDavid210@users.noreply.github.com>
2023-10-30 21:39:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,12 @@
 function setup() {
-    let header = createHeader();
-    header.style('background-color', '#333');
-    header.style('color', '#fff');
-    header.style('text-align', 'center');
-    header.style('padding', '20px 0');
-    header.html('<h1>My Portfolio</h1>');
-  
-    let nav = createNav();
-    nav.style('text-align', 'center');
-  
-    let links = selectAll('.links a');
-    links.forEach(link => {
-        link.style('display', 'inline-block');
-        link.style('padding', '10px 20px');
-        link.style('margin', '0 5px');
-        link.style('text-decoration', 'none');
-        link.style('color', '#fff');
-        link.style('background-color', '#555');
-        link.style('border', '2px solid #555');
-        link.style('border-radius', '5px');
-        link.style('transition', 'background-color 0.3s, border-color 0.3s');
-    });
-  
-    links.forEach(link => {
-        link.mouseOver(() => {
-            link.style('background-color', '#fff');
-            link.style('color', '#333');
-            link.style('border-color', '#fff');
-        });
-      
-        link.mouseOut(() => {
-            link.style('background-color', '#555');
-            link.style('color', '#fff');
-            link.style('border-color', '#555');
-        });
-    });
+  createCanvas(1000, 200);
+  textAlign(CENTER, CENTER);
+  textSize(64);
+  textFont('Brush Script MT');
 }
 
-function createHeader() {
-    return createDiv('').parent('body');
-}
-
-function createNav() {
-    return createDiv('').parent('body');
+function draw() {
+  background(0);
+  fill(255); 
+  text('Matthew David', width / 2, height / 2);
 }
